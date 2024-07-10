0b1b02bf3a89cd1d2a5a8f5dd70273a56c194eb6
drowsy <away28618@gmail.com>
2024-07-10 21:03:37 +0530

    added remaining logic and completed the conversion logic

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import InputBox from "./components/InputBox";
+import "./App.css";
+import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const currencyOptions = Object.keys(currencyInfo);
+
+  const swap = function () {
+    setFrom(to);
+    setTo(from);
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+  };
+
+  const convertAmount = (amount) => {
+    console.log(amount);
+    console.log(currencyInfo[to]);
+    return setConvertedAmount(amount * currencyInfo[to]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <h1 className="text-2xl bg-red-600">Currency converter</h1> */}
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://imgs.search.brave.com/Uirc6ktPndqBShx7nzat_qnv1HTIKzM1qSMn1fFDQr0/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxMi8w/Mi8yOC8xMC8xOS9i/dXNpbmVzcy0xODEw/N182NDAuanBn')`,
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convertAmount(amount);
+              }}
+            >
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={currencyOptions}
+                  onAmountChange={(amount) => setAmount(amount)}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  selectCurrency={from}
+                />
+              </div>
+              <div className="relative w-full h-0.5">
+                <button
+                  type="button"
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                  onClick={swap}
+                >
+                  swap
+                </button>
+              </div>
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOptions={currencyOptions}
+                  onAmountChange={(convertedAmount) =>
+                    setConvertedAmount(convertedAmount)
+                  }
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
